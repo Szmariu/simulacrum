ce17723ee6169db7ae2ddb3a467ebad21c56f155
Michał Szałański <szmariu.miki@gmail.com>
2020-03-28 13:26:10 +0100

    Add a plot with %

--- code.R
+++ code.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(MonteCarlo)
 library(ggplot2)
 library(pbapply)
+library(scales)
 
 source('code/objects.R')
 source('code/functions.R')
@@ -20,8 +21,8 @@ defender <- makeDefender(
   wounds = 20,
   armour = 4,
   dodge = 30,
-  forceField = 10,
-  forceFieldOverload = 5,
+  forceField = 0,
+  forceFieldOverload = 0,
   isForceFieldOverloaded = 0
 )
 
@@ -31,12 +32,42 @@ singleAttack()
 a <- pbreplicate(10000, singleAttack())
 
 a %>%
-  table() %>%
-  as.data.frame() %>%
-  ggplot() +
-  geom_col(aes(x = ., y = Freq))
+  as_tibble() %>%
+  ggplot(aes(x = as.factor(value))) +
+  geom_bar(
+    aes(y = (..count..)/sum(..count..)),
+    fill = '#378CC7') +
+  geom_text(
+    aes(y = ((..count..)/sum(..count..)),
+        label = scales::percent((..count..)/sum(..count..))),
+    stat = "count",
+    vjust = -0.75) +
+  scale_y_continuous(labels = percent) +
+  labs(x = 'Damage to target', y = 'Probability')
+
+# Filter 0
+a %>%
+  as_tibble() %>%
+  filter(value > 0) %>%
+  ggplot(aes(x = as.factor(value))) +
+    geom_bar(
+      aes(y = (..count..)/sum(..count..)),
+      fill = '#378CC7') +
+    geom_text(
+      aes(y = ((..count..)/sum(..count..)),
+      label = scales::percent((..count..)/sum(..count..))),
+      stat = "count",
+      vjust = -0.75) +
+    scale_y_continuous(labels = percent) +
+    labs(x = 'Damage to target', y = 'Probability')
 
 a %>%
   as_tibble() %>%
   ggplot() +
-  geom_histogram(aes(x = value), fill = '#378CC7')
+  geom_histogram(aes(x = value), fill = '#378CC7') 
+
+a %>%
+  table() %>%
+  as.data.frame() %>%
+  ggplot() +
+  geom_col(aes(x = ., y = Freq), fill = '#378CC7')
