ddf33d03bb5aa9dc1b53ba86bd63af1e02793c8d
Michał Szałański <michal.szalanski1@gmail.com>
2020-06-02 16:09:10 +0200

    Make most of the inputs 50% width, to use space more efficiently

--- ui.R
+++ ui.R
@@ -29,27 +29,45 @@ shinyUI(
                              display: block;'),
                 tabsetPanel(type = "tabs",
                             tabPanel("Attacker",
-                                     numericInput('BS_', 'BS', 50, 0, 100, 5),
-                                     numericInput('damageDie_', "Weapon's damage die (e.g. d10)", 10, 0, 20, 1),
-                                     numericInput('damageModifier_', "Weapon's damage modifier", 9, 0, 100, 1),
-                                     numericInput('penetration_', "Weapon's penetration", 4, 0, 100, 1),
+                                     splitLayout(
+                                         numericInput('BS_', 'Ballistic skill', 50, 0, 100, 5),
+                                         numericInput('damageDie_', "Weapon's damage die (e.g. d10)", 10, 0, 20, 1)
+                                     ),
+                                     
+                                     splitLayout(
+                                         numericInput('damageModifier_', "Weapon's damage modifier", 9, 0, 100, 1),
+                                         numericInput('penetration_', "Weapon's penetration", 4, 0, 100, 1)
+                                     ),
+                                     
                                      numericInput('rateOfFire_', "Weapon's rate of fire (input only the one used, usually highest)", 3, 0, 100, 5),
-                                     radioButtons('firingMode', "Weapon's type of fire", choiceNames = c('Single', 'Semi auto', 'Full auto'), choiceValues = c('single', 'semi', 'full'), selected = 'full'),
-                                     checkboxGroupInput('additional_attacker', "Additional features (attacker)", choiceNames = c('Tearing', 'Proven'), choiceValues = c('tearing', 'proven'), selected = c('tearing')),
+                                     
+                                     splitLayout(
+                                         radioButtons('firingMode', "Weapon's type of fire", choiceNames = c('Single', 'Semi auto', 'Full auto'), choiceValues = c('single', 'semi', 'full'), selected = 'full'),
+                                         checkboxGroupInput('additional_attacker', "Additional features (attacker)", choiceNames = c('Tearing', 'Proven'), choiceValues = c('tearing', 'proven'), selected = c('tearing'))
+                                     ),
+
                                      conditionalPanel(
                                          condition = "input.additional_attacker.includes('proven')",
                                          numericInput('proven_', "Round damage values lower than:", 0, 0, 10, 1))
                                      ),
                             tabPanel("Defender", 
-                                     numericInput('T_', 'T', 30, 0, 100, 5),
-                                     numericInput('wounds_', "Wounds", 20, 0, 100, 1),
-                                     numericInput('armour_', "Armour", 4, 0, 100, 1),
-                                     numericInput('dodge_', 'Dodge', 30, 0, 100, 5),
-                                     numericInput('forceField_', "Force field", 0, 0, 100, 5),
-                                     numericInput('forceFieldOverload_', "Force field overload", 0, 0, 100, 1)
+                                     splitLayout(
+                                         numericInput('T_', 'Toughness', 30, 0, 100, 5),
+                                         numericInput('wounds_', "Wounds", 20, 0, 100, 1)
+                                     ),
+                                     
+                                     splitLayout(
+                                         numericInput('armour_', "Armour", 4, 0, 100, 1),
+                                         numericInput('dodge_', 'Dodge', 30, 0, 100, 5)
+                                     ),
+                                     
+                                     splitLayout(
+                                         numericInput('forceField_', "Force field", 0, 0, 100, 5),
+                                         numericInput('forceFieldOverload_', "Force field overload", 0, 0, 100, 1)
+                                     )
                                      ),
                             tabPanel("Settings",
-                                     numericInput('nReps_', 'Number of monte carlo rounds', 1000, 100, 10000000, 10000),
+                                     numericInput('nReps_', 'Number of monte carlo rounds', 4000, 1000, 50000, 1000),
                                      checkboxInput('showMisses_', 'Show misses on the plot?', 0)
                                     )
                             ),
